97c15d550e6238ec35af65c6d02a1dae978d780c
braulov <mr.braulov@mail.ru>
2024-10-27 02:21:16 +0300

    00-ps.38

--- 00-ps/solution.c
+++ 00-ps/solution.c
@@ -7,8 +7,8 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-#define MAX_ARGS 1050
-#define MAX_LENGTH_ARG 1900
+#define MAX_ARGS 1025
+#define MAX_LENGTH_ARG 2000
 #define MAX_LENGTH_PATH 256
 void split(char* path, char** args) {
     FILE *file = fopen(path,"rb");
