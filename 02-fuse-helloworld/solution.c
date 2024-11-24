02242561bbc10ada00aa550c4aff387dfb35b915
braulov <mr.braulov@mail.ru>
2024-11-24 23:36:33 +0300

    v1

--- 02-fuse-helloworld/solution.c
+++ 02-fuse-helloworld/solution.c
@@ -1,13 +1,66 @@
 #include <solution.h>
-
 #include <fuse.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+static int hw_getattr(const char *path, struct stat *stbuf, struct fuse_file_info* fi) {
+    (void) fi;
+    memset(stbuf, 0, sizeof(struct stat));
 
+    stbuf->st_uid = getuid();
+    stbuf->st_gid = getgid();
+
+
+    if (strcmp(path, "/") == 0) {  // Проверка, если путь корневой
+        stbuf->st_mode = S_IFDIR | 0775;
+        stbuf->st_nlink = 2;
+    } else if (strcmp(path, "../") == 0){
+        stbuf->st_mode = S_IFDIR | 0755;
+        stbuf->st_nlink = 2;
+    } else if (strcmp(path, "/hello") == 0) {
+        stbuf->st_mode = S_IFREG | 0400;
+        stbuf->st_nlink = 1;
+        stbuf->st_size = 20;
+    }
+      else {
+        return -ENOENT;  // Файл не найден
+    }
+
+    return 0;
+}
+static int hw_readdir( const char *path, void *buffer, fuse_fill_dir_t filler, off_t, struct fuse_file_info *,enum fuse_readdir_flags) {
+
+    filler( buffer, ".", NULL, 0,0);
+    filler( buffer, "..", NULL, 0,0);
+    if ( strcmp( path, "/" ) == 0 ) // If the user is trying to show the files/directories of the root directory show the following
+    {
+        filler( buffer, "hello", NULL, 0,0);
+    }
+
+    return 0;
+}
+static int hw_read( const char *path, char *buffer, size_t size, off_t offset, struct fuse_file_info *) {
+    char helloText[] = "hello";
+    char* selectedText = NULL;
+    if ( strcmp( path, "/hello" ) == 0 )
+        selectedText = helloText;
+    else
+        return -1;
+    memcpy( buffer, selectedText + offset, size );
+    return strlen( selectedText ) - offset;
+}
 static const struct fuse_operations hellofs_ops = {
-	/* implement me */
+	.getattr = hw_getattr,
+    .readdir = hw_readdir,
+    //.open = hw_open,
+    .read = hw_read,
 };
-
 int helloworld(const char *mntp)
 {
-	char *argv[] = {"exercise", "-f", (char *)mntp, NULL};
+	char *argv[] = {"./a.out", "-f", (char *)mntp, NULL};
 	return fuse_main(3, argv, &hellofs_ops, NULL);
 }
--- 14-ext2-blkiter/solution.c
+++ 14-ext2-blkiter/solution.c
@@ -1,51 +1,163 @@
 #include <solution.h>
 #include <fs_malloc.h>
+#include <ext2fs/ext2fs.h>
+#include <ext2fs/ext2_fs.h>
+#include <errno.h>
+#include <unistd.h>
 
 #include <errno.h>
 
-struct ext2_fs
-{
-	/* implement me */
+struct ext2_fs {
+    int file_descriptor;
+    int blk_size;
+    struct ext2_super_block super_block;
 };
 
-struct ext2_blkiter
-{
-	/* implement me */
+struct ext2_blkiter {
+    struct ext2_fs *filesystem;
+    int inode_table_offset;
+
+    struct ext2_inode current_inode;
+    int iterator_position;
+
+    int *indirect_blk_buffer;
+    int *double_indirect_blk_buffer;
+    int active_indirect_block;
 };
 
-int ext2_fs_init(struct ext2_fs **fs, int fd)
-{
-	/* implement me */
-	(void) fs;
-	(void) fd;
-	return -EOPNOTSUPP;
+int ext2_fs_init(struct ext2_fs **fs, int fd) {
+    struct ext2_fs *temp_fs = fs_xmalloc(sizeof(struct ext2_fs));
+    temp_fs->file_descriptor = fd;
+
+    ssize_t read_result = pread(temp_fs->file_descriptor, &temp_fs->super_block, SUPERBLOCK_SIZE, SUPERBLOCK_OFFSET);
+    if (read_result == -1) {
+        fs_xfree(temp_fs);
+        return -errno;
+    }
+
+    temp_fs->blk_size = EXT2_BLOCK_SIZE(&temp_fs->super_block);
+    *fs = temp_fs;
+
+    return 0;
 }
 
-void ext2_fs_free(struct ext2_fs *fs)
-{
-	/* implement me */
-	(void) fs;
+void ext2_fs_free(struct ext2_fs *fs) {
+    fs_xfree(fs);
 }
 
-int ext2_blkiter_init(struct ext2_blkiter **i, struct ext2_fs *fs, int ino)
-{
-	/* implement me */
-	(void) i;
-	(void) fs;
-	(void) ino;
-	return -EOPNOTSUPP;
+int ext2_blkiter_init(struct ext2_blkiter **iter, struct ext2_fs *fs, int inode_number) {
+    size_t descs_per_block = fs->blk_size / sizeof(struct ext2_group_desc);
+    size_t inode_group_idx = (inode_number - 1) / fs->super_block.s_inodes_per_group;
+    size_t inode_idx_in_group = (inode_number - 1) % fs->super_block.s_inodes_per_group;
+    size_t group_desc_offset = fs->blk_size * (fs->super_block.s_first_data_block + 1 + (inode_group_idx / descs_per_block));
+    size_t group_desc_within_block = (inode_group_idx % descs_per_block) * sizeof(struct ext2_group_desc);
+
+    struct ext2_group_desc group_desc;
+    ssize_t read_group = pread(fs->file_descriptor, &group_desc, sizeof(group_desc), group_desc_offset + group_desc_within_block);
+    if (read_group == -1) {
+        return -errno;
+    }
+
+    struct ext2_blkiter *temp_iter = fs_xmalloc(sizeof(struct ext2_blkiter));
+    temp_iter->inode_table_offset = group_desc.bg_inode_table;
+
+    ssize_t read_inode_data = pread(fs->file_descriptor, &temp_iter->current_inode, sizeof(struct ext2_inode),
+                                    fs->blk_size * temp_iter->inode_table_offset + inode_idx_in_group * fs->super_block.s_inode_size);
+    if (read_inode_data == -1) {
+        fs_xfree(temp_iter);
+        return -errno;
+    }
+
+    temp_iter->iterator_position = 0;
+    temp_iter->filesystem = fs;
+
+    temp_iter->indirect_blk_buffer = NULL;
+    temp_iter->double_indirect_blk_buffer = NULL;
+    temp_iter->active_indirect_block = -1;
+
+    *iter = temp_iter;
+
+    return 0;
 }
 
-int ext2_blkiter_next(struct ext2_blkiter *i, int *blkno)
-{
-	/* implement me */
-	(void) i;
-	(void) blkno;
-	return 0;
+int ext2_blkiter_next(struct ext2_blkiter *iter, int *block_number) {
+    int block_pointers_per_block = iter->filesystem->blk_size / sizeof(int);
+
+    int direct_blocks_end = EXT2_NDIR_BLOCKS;
+    int single_indirect_start = direct_blocks_end;
+    int single_indirect_end = single_indirect_start + block_pointers_per_block;
+    int double_indirect_start = single_indirect_end;
+    int double_indirect_end = double_indirect_start + block_pointers_per_block * block_pointers_per_block;
+
+    if (iter->iterator_position < direct_blocks_end) {
+        int blk_ptr = iter->current_inode.i_block[iter->iterator_position];
+        if (blk_ptr == 0) {
+            return 0;
+        }
+
+        *block_number = blk_ptr;
+        iter->iterator_position++;
+        return 1;
+    }
+
+    if (iter->iterator_position < single_indirect_end) {
+        if (!iter->indirect_blk_buffer) {
+            iter->indirect_blk_buffer = fs_xmalloc(iter->filesystem->blk_size);
+            ssize_t result = pread(iter->filesystem->file_descriptor, iter->indirect_blk_buffer, iter->filesystem->blk_size,
+                                   iter->current_inode.i_block[EXT2_IND_BLOCK] * iter->filesystem->blk_size);
+            if (result == -1) {
+                return -errno;
+            }
+        }
+
+        int indirect_idx = iter->iterator_position - single_indirect_start;
+        if (iter->indirect_blk_buffer[indirect_idx] == 0) {
+            return 0;
+        }
+
+        *block_number = iter->indirect_blk_buffer[indirect_idx];
+        iter->iterator_position++;
+        return 1;
+    }
+
+    if (iter->iterator_position < double_indirect_end) {
+        if (!iter->double_indirect_blk_buffer) {
+            iter->double_indirect_blk_buffer = fs_xmalloc(iter->filesystem->blk_size);
+            ssize_t result = pread(iter->filesystem->file_descriptor, iter->double_indirect_blk_buffer,
+                                   iter->filesystem->blk_size,
+                                   iter->current_inode.i_block[EXT2_DIND_BLOCK] * iter->filesystem->blk_size);
+            if (result == -1) {
+                return -errno;
+            }
+        }
+
+        int outer_idx = (iter->iterator_position - double_indirect_start) / block_pointers_per_block;
+        int inner_idx = (iter->iterator_position - double_indirect_start) % block_pointers_per_block;
+
+        if (iter->active_indirect_block != iter->double_indirect_blk_buffer[outer_idx]) {
+            iter->active_indirect_block = iter->double_indirect_blk_buffer[outer_idx];
+            if (pread(iter->filesystem->file_descriptor, iter->indirect_blk_buffer, iter->filesystem->blk_size,
+                      iter->active_indirect_block * iter->filesystem->blk_size) == -1) {
+                return -errno;
+            }
+        }
+
+        if (iter->indirect_blk_buffer[inner_idx] == 0) {
+            return 0;
+        }
+
+        *block_number = iter->indirect_blk_buffer[inner_idx];
+        iter->iterator_position++;
+        return 1;
+    }
+
+    return 0;
 }
 
-void ext2_blkiter_free(struct ext2_blkiter *i)
-{
-	/* implement me */
-	(void) i;
+void ext2_blkiter_free(struct ext2_blkiter *iter) {
+    if (iter) {
+        fs_xfree(iter->indirect_blk_buffer);
+        fs_xfree(iter->double_indirect_blk_buffer);
+        fs_xfree(iter);
+    }
 }
